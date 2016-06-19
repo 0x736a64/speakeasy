44018e7eee47789d0fb8111548ca47ad50d2c061
blurdsss <6139497b@opayq.com>
2016-06-19 16:19:29 -0400

    refactor and edit formatting

--- speakeasy.go
+++ speakeasy.go
@@ -1,53 +1,44 @@
 package main
 
 import (
-    "fmt"
-    "flag"
-    "log"
-    "strings"
-    "net/http"
-    "github.com/kardianos/osext"
-	"github.com/skratchdot/open-golang/open"
+  "flag"
+  "fmt"
+  "log"
+  "strings"
+  "net/http"
+  "github.com/kardianos/osext"
+  "github.com/skratchdot/open-golang/open"
 )
 
 func main() {
-    dir := getDir()
-    port := getPort()
-    host := getHost(port)
-    run(dir, port, host)
+  dir := getDir()
+  port := getPort()
+  host := getHost(port)
+  run(dir, port, host)
 }
 
 func getDir() string {
-    dir, err := osext.ExecutableFolder()
-    if err != nil {
-        log.Fatal(err)
-    }
-    return dir
+  dir, err := osext.ExecutableFolder()
+  if err != nil {
+    log.Fatal(err)
+  }
+  return dir
+}
+
+func getHost(port string) string {
+  return strings.TrimSpace(fmt.Sprintf("http://localhost%s", port))
 }
 
 func getPort() string {
-    portInput := flag.String("port", "3000", "an open port to run the HTTP server")
-    flag.Parse()
-    return fmt.Sprintf(":%s", *portInput)
-}
-
-func getHost (port string) string {
-   return strings.TrimSpace(fmt.Sprintf("http://localhost%s", port))   
-}
-
-func setOutput (host string) {
-    fmt.Println("-> Listening on ", host)
-    fmt.Println("-> Press ctrl-c to kill process")
-}
-
-func setRoot (dir string) {
-    root := http.FileServer(http.Dir(dir))
-    http.Handle("/", root)
+  portInput := flag.String("port", "3000", "an open port to run the HTTP server")
+  flag.Parse()
+  return fmt.Sprintf(":%s", *portInput)
 }
 
 func run(dir, port, host string) {
-    setRoot(dir)
-    setOutput(host)
-    open.Start(host)
-    http.ListenAndServe(port, nil)
+  open.Start(host)
+  fmt.Println("-> Listening on ", host)
+  fmt.Println("-> Press ctrl-c to kill process")
+  http.CloseIdleConnections();
+  log.Fatal(http.ListenAndServe(port, http.FileServer(http.Dir(dir))))
 }
