ee6c1d8ec01f550d7be7373d6c3d4ca884389fc6
blurdsss <6139497b@opayq.com>
2016-06-19 16:26:29 -0400

    format tabs

--- speakeasy.go
+++ speakeasy.go
@@ -1,44 +1,44 @@
 package main
 
 import (
-	"flag"
-	"fmt"
-	"log"
-	"strings"
-	"net/http"
+  "flag"
+  "fmt"
+  "log"
+  "strings"
+  "net/http"
   "github.com/kardianos/osext"
   "github.com/skratchdot/open-golang/open"
 )
 
 func main() {
-	dir := getDir()
-	port := getPort()
-	host := getHost(port)
-	run(dir, port, host)
+  dir := getDir()
+  port := getPort()
+  host := getHost(port)
+  run(dir, port, host)
 }
 
 func getDir() string {
-	dir, err := osext.ExecutableFolder()
-	if err != nil {
-		log.Fatal(err)
-	}
-	return dir
+  dir, err := osext.ExecutableFolder()
+  if err != nil {
+  	log.Fatal(err)
+  }
+  return dir
 }
 
 func getHost(port string) string {
-	return strings.TrimSpace(fmt.Sprintf("http://localhost%s", port))
+  return strings.TrimSpace(fmt.Sprintf("http://localhost%s", port))
 }
 
 func getPort() string {
-	portInput := flag.String("port", "3000", "an open port to run the HTTP server")
-	flag.Parse()
-	return fmt.Sprintf(":%s", *portInput)
+  portInput := flag.String("port", "3000", "an open port to run the HTTP server")
+  flag.Parse()
+  return fmt.Sprintf(":%s", *portInput)
 }
 
 func run(dir, port, host string) {
-	open.Start(host)
-	fmt.Println("-> Listening on ", host)
-	fmt.Println("-> Press ctrl-c to kill process")
+  open.Start(host)
+  fmt.Println("-> Listening on ", host)
+  fmt.Println("-> Press ctrl-c to kill process")
   http.CloseIdleConnections();
-	log.Fatal(http.ListenAndServe(port, http.FileServer(http.Dir(dir))))
+  log.Fatal(http.ListenAndServe(port, http.FileServer(http.Dir(dir))))
 }
