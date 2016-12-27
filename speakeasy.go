eadaba7eb3437c9b160e1b9ff2ba8a166e289608
0x736164 <1a2e1ef0@opayq.com>
2016-12-27 02:40:28 -0500

    add cache-control response header, update formatting

--- speakeasy.go
+++ speakeasy.go
@@ -1,44 +1,52 @@
 package main
 
 import (
-  "flag"
-  "fmt"
-  "log"
-  "strings"
-  "net/http"
-  "github.com/kardianos/osext"
-  "github.com/skratchdot/open-golang/open"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/kardianos/osext"
+	"github.com/skratchdot/open-golang/open"
 )
 
 func main() {
-  dir := getDir()
-  port := getPort()
-  host := getHost(port)
-  run(dir, port, host)
+	dir := getDir()
+	port := getPort()
+	host := getHost(port)
+	run(dir, port, host)
 }
 
 func getDir() string {
-  dir, err := osext.ExecutableFolder()
-  if err != nil {
-  	log.Fatal(err)
-  }
-  return dir
+	dir, err := osext.ExecutableFolder()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return dir
 }
 
 func getHost(port string) string {
-  return strings.TrimSpace(fmt.Sprintf("http://localhost%s", port))
+	return strings.TrimSpace(fmt.Sprintf("http://localhost%s", port))
 }
 
 func getPort() string {
-  portInput := flag.String("port", "3000", "an open port to run the HTTP server")
-  flag.Parse()
-  return fmt.Sprintf(":%s", *portInput)
+	portInput := flag.String("port", "3000", "an open port to run the HTTP server")
+	flag.Parse()
+	return fmt.Sprintf(":%s", *portInput)
+}
+
+func setHeadersThenServe(handler http.Handler) http.HandlerFunc {
+	return func(server http.ResponseWriter, request *http.Request) {
+		server.Header().Add("Cache-Control", "no-store")
+		handler.ServeHTTP(server, request)
+	}
 }
 
 func run(dir, port, host string) {
-  open.Start(host)
-  fmt.Println("-> Listening on ", host)
-  fmt.Println("-> Press ctrl-c to kill process")
-  http.CloseIdleConnections();
-  log.Fatal(http.ListenAndServe(port, http.FileServer(http.Dir(dir))))
+	open.Start(host)
+	fmt.Println("-> Listening on ", host)
+	fmt.Println("-> Press ctrl-c to kill process")
+	http.Handle("/", setHeadersThenServe(http.FileServer(http.Dir(dir))))
+	panic(http.ListenAndServe(port, nil))
 }
